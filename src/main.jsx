c90ff3f68a29ba6e58d24a29f801f13bab405332
nuttawat25243 <nuttawatsaepan@gmail.com>
2023-10-09 14:45:27 +0700

    back to og

--- src/main.jsx
+++ src/main.jsx
@@ -1,108 +1,51 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import {
-//   createBrowserRouter,
-//   RouterProvider
-// } from "react-router-dom";
-// import "./index.css";
-// import Home from './pages/Home.jsx';
-// import ProductFilter from './pages/ProductAll.jsx'
-// import Productdetail from './pages/Productdetail.jsx'
-// import ProductTop from './pages/ProductTop.jsx'
-// import ProductBottom from './pages/ProductBottom.jsx'
-// import ProductAcc from './pages/ProductAcc.jsx'
-// import CheckOut from './pages/CheckOut.jsx'
-
-// import { Provider } from 'react-redux';
-// import store from '../src/store/store.js';
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home/>,
-//   },
-//   {
-//     path: "/product",
-//     element: <ProductFilter/>,
-//   },
-//    {
-//      path: "/product/:id",
-//     element: <Productdetail/>,
-//    },
-//    {
-//     path: "/product/tops",
-//    element: <ProductTop/>,
-//   },
-//   {
-//     path: "/product/bottoms",
-//    element: <ProductBottom/>,
-//   },
-//   {
-//     path: "/product/accessories",
-//    element: <ProductAcc/>,
-//   },
-//   {
-//     path: "/checkout",
-//    element: <CheckOut/>,
-//   },
-// ]);
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//      <RouterProvider router={router} />
-//      </Provider>
-//   </React.StrictMode>,
-// )
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {
-  createHashRouter,
+  createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
 import "./index.css";
 import Home from './pages/Home.jsx';
-// import ProductFilter from './pages/ProductAll.jsx'
-// import Productdetail from './pages/Productdetail.jsx'
-// import ProductTop from './pages/ProductTop.jsx'
-// import ProductBottom from './pages/ProductBottom.jsx'
-// import ProductAcc from './pages/ProductAcc.jsx'
-// import CheckOut from './pages/CheckOut.jsx'
+import ProductFilter from './pages/ProductAll.jsx'
+import Productdetail from './pages/Productdetail.jsx'
+import ProductTop from './pages/ProductTop.jsx'
+import ProductBottom from './pages/ProductBottom.jsx'
+import ProductAcc from './pages/ProductAcc.jsx'
+import CheckOut from './pages/CheckOut.jsx'
 
 import { Provider } from 'react-redux';
 import store from '../src/store/store.js';
 
-const router = createHashRouter ([
+const router = createBrowserRouter([
   {
-    path: "/*",
+    path: "/",
     element: <Home/>,
   },
-  // {
-  //   path: "/product",
-  //   element: <ProductFilter/>,
-  // },
-  //  {
-  //    path: "/product/:id",
-  //   element: <Productdetail/>,
-  //  },
-  //  {
-  //   path: "/product/tops",
-  //  element: <ProductTop/>,
-  // },
-  // {
-  //   path: "/product/bottoms",
-  //  element: <ProductBottom/>,
-  // },
-  // {
-  //   path: "/product/accessories",
-  //  element: <ProductAcc/>,
-  // },
-  // {
-  //   path: "/checkout",
-  //  element: <CheckOut/>,
-  // },
+  {
+    path: "/product",
+    element: <ProductFilter/>,
+  },
+   {
+     path: "/product/:id",
+    element: <Productdetail/>,
+   },
+   {
+    path: "/product/tops",
+   element: <ProductTop/>,
+  },
+  {
+    path: "/product/bottoms",
+   element: <ProductBottom/>,
+  },
+  {
+    path: "/product/accessories",
+   element: <ProductAcc/>,
+  },
+  {
+    path: "/checkout",
+   element: <CheckOut/>,
+  },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
